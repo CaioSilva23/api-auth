ae7890a7df5bef36fb19619aa62e1dfb9dcaf4e5
caio silva <caio_ceac23@hotmail.com>
2023-06-18 01:18:30 -0300

    url static

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -5,6 +5,9 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +33,6 @@ urlpatterns += [
    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
