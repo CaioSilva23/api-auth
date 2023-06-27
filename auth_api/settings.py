2903d7f0d59c1a37d73ec0d1ad61aa875e04383e
caio silva <caio_ceac23@hotmail.com>
2023-06-27 18:04:56 -0300

    up

--- auth_api/settings.py
+++ auth_api/settings.py
@@ -36,9 +36,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -169,12 +169,18 @@ SWAGGER_SETTINGS = {
 
 PASSWORD_RESET_TIMEOUT = 900  # 900sec = 15 Min
 
-CORS_ALLOWED_ORIGINS = [
+# CORS_ALLOWED_ORIGINS = [
+#     "https://api-auth.up.railway.app",
+#     "http://127.0.0.1:8000",
+# ]
+
+CSRF_TRUSTED_ORIGINS = [
     "https://api-auth.up.railway.app",
-    "http://127.0.0.1:8000",
 ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # EMAIL
 
